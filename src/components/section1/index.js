8e0fca04a0fbfc6772935f62239df5b9ea62d388
Dito Bayu Adhitya <ditobayuadhitya@Ditos-MacBook-Air.local>
2026-01-20 08:10:33 +0700

    feat: auto commit

--- src/components/section1/index.js
+++ src/components/section1/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import foto from "../../assets/foto.JPG";
+import foto from "../../assets/foto.jpeg";
 
 gsap.registerPlugin(ScrollTrigger);
 
